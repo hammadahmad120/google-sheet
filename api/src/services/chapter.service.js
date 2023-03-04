17797ba86d9c1102285abff241df435790a42fa9
hammadahmad120 <hammad.shahid120@gmail.com>
2023-03-04 21:03:09 +0500

    added api

--- api/src/services/chapter.service.js
+++ api/src/services/chapter.service.js
@@ -1,4 +1,10 @@
+const {google} = require('googleapis');
 
+const auth = new google.auth.GoogleAuth({
+  keyFile: "keys.json", //the key file
+  //url to spreadsheets API
+  scopes: "https://www.googleapis.com/auth/spreadsheets", 
+});
 module.exports = {
 getAllChapters: async () => {
   // var response = await db.Questions.findAll({
@@ -9,7 +15,17 @@ getAllChapters: async () => {
   // });
   // return response;
 },
-getChapterById: async(chapterId) =>{
+getChapterById: async(chapterId) => {
+  const authClientObject = await auth.getClient();
+  const googleSheetsInstance = google.sheets({ version: "v4", auth: authClientObject });
+  const request = {
+    spreadsheetId: '1Bk0uXXTYhBom2d3hN2WO7Eyo5bwPHjtR_zS2n0mSUis',
+    ranges: [],
+    includeGridData: false
+  };
+  let res = await googleSheetsInstance.spreadsheets.get(request);
+  console.log("Response is: ", res);
+  //googleSheetsInstance.spreadsheets.get()
   return {
     id: '1',
     name: 'chap'
